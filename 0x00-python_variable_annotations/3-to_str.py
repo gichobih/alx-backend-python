c7d724f7aa0ed92693b4cfc39e3272783c323124
gichobih <dennismuri254@gmail.com>
2024-10-11 10:14:01 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_str"""
+
+def to_str(n: float) ->str:
+    """Takes a float n and return string presentation of the float"""
+    return str(n)
+
