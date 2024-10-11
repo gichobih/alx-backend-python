95b100a7aa8927a5d11999b84727ddc37199915c
gichobih <dennismuri254@gmail.com>
2024-10-11 09:49:57 -0700

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """Takes a string str1 and a string str2 as 
-    arguments and returns a concatenated string"""
+    """Takes two strings and  return concetrated strings"""
     return str1 + str2
